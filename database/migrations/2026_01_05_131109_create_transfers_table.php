0dcf3ef951472e63e6efc0cf8635da59c883fd28
Gilbert Engalan <gilbertpude2003@gmail.com>
2026-01-05 21:16:56 +0800

    add migrations

---
+++ database/migrations/2026_01_05_131109_create_transfers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('transfers', function (Blueprint $table) {
+             $table->id();
+            $table->foreignId('item_id')->constrained()->onDelete('cascade');
+            $table->foreignId('from_location_id')->constrained('locations')->onDelete('cascade');
+            $table->foreignId('to_location_id')->constrained('locations')->onDelete('cascade');
+            $table->integer('quantity');
+            $table->string('status')->default('pending'); // pending, completed, cancelled
+            $table->text('notes')->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamp('completed_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('transfers');
+    }
+};
